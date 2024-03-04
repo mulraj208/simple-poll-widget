9628e61d2903ef79973ed92cea94c30b40f33092
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-03-04 12:59:58 +0530

    feat: add logic to display unique poll widget

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
-import '../css/style.css'
+import '../css/style.css';
 
 window.addEventListener('load', function () {
     function setObjectEmbedHeight() {
         const objectElements = document.querySelectorAll(".object-embed");
+        const embeddedElementStack = new Set();
 
-        objectElements.forEach((objectElement) => {
-            const embeddedDoc = objectElement.contentDocument;
+        objectElements.forEach((objectEmbedElement) => {
+            const embeddedDoc = objectEmbedElement.contentDocument;
+            const dataAttribute = objectEmbedElement.getAttribute('data');
 
-            if (embeddedDoc) {
-                const height = embeddedDoc.documentElement.scrollHeight;
-
-                objectElement.style.height = `${height}px`;
+            // If the same data attribute is detected, hide the embedded element
+            if (embeddedElementStack.has(dataAttribute)) {
+                objectEmbedElement.style.display = 'none';
+            } else {
+                if (embeddedDoc) {
+                    const height = embeddedDoc.documentElement.scrollHeight;
+                    objectEmbedElement.style.height = `${height}px`;
+                }
             }
-        })
+
+            embeddedElementStack.add(dataAttribute);
+        });
     }
 
-    setObjectEmbedHeight()
-})
+    setObjectEmbedHeight();
+});
